7b4013109cd6a867a9a7541053f1f385bd1bcb8d
theun1c <belovdima1122@yandex.ru>
2026-03-14 16:26:41 +0300

    feat: add admin ui

---
+++ src/Automata.Desktop/Views/AdministrationPage.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Automata.Desktop.Views;
+
+public partial class AdministrationPage : UserControl
+{
+    public AdministrationPage()
+    {
+        InitializeComponent();
+    }
+}
