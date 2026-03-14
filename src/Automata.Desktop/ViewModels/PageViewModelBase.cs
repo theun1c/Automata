edac455841e49550301ba85daee3bbb12e247d9c
theun1c <belovdima1122@yandex.ru>
2026-03-14 16:25:03 +0300

    feat: add page vm base

---
+++ src/Automata.Desktop/ViewModels/PageViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automata.Desktop.ViewModels
+{
+    public abstract class PageViewModelBase : ViewModelBase
+    {
+        public abstract string Title { get; }
+
+    }
+}
