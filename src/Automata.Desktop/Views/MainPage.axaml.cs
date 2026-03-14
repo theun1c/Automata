67cf3225b149c861f19413f20d1dff684e89dd66
theun1c <belovdima1122@yandex.ru>
2026-03-14 16:27:19 +0300

    feat: add main page ui

---
+++ src/Automata.Desktop/Views/MainPage.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Automata.Desktop.Views;
+
+public partial class MainPage : UserControl
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
