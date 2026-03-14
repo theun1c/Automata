20d1133b29efa4a357781b69689433703dd3e86c
theun1c <belovdima1122@yandex.ru>
2026-03-14 16:23:32 +0300

    feat: add Admin vm

---
+++ src/Automata.Desktop/ViewModels/AdministrationPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automata.Desktop.ViewModels
+{
+    public class AdministrationPageViewModel : PageViewModelBase
+    {
+        public override string Title => "Администрирование";
+
+    }
+}
