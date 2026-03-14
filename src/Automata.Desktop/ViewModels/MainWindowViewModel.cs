16eb95ce48d2b9e09976e0f4100b47844abb630c
theun1c <belovdima1122@yandex.ru>
2026-03-14 16:29:04 +0300

    feat: add main page vm

--- src/Automata.Desktop/ViewModels/MainWindowViewModel.cs
+++ src/Automata.Desktop/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,25 @@
-﻿namespace Automata.Desktop.ViewModels
+﻿using System.Collections.ObjectModel;
+
+namespace Automata.Desktop.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+        public ObservableCollection<PageViewModelBase> Pages { get; } = new();
+
+        private PageViewModelBase? _selectedPage;
+
+        public PageViewModelBase? SelectedPage
+        {
+            get => _selectedPage;
+            set => SetProperty(ref _selectedPage, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            Pages.Add(new MainPageViewModel());
+            Pages.Add(new AdministrationPageViewModel());
+
+            SelectedPage = Pages[0];
+        }
     }
 }
