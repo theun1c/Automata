6bc244e0cb62129d5be15f7c4af7c214ba30ebed
theun1c <belovdima1122@yandex.ru>
2026-03-14 16:24:00 +0300

    feat: add main page vm

---
+++ src/Automata.Desktop/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automata.Desktop.ViewModels
+{
+    public class MainPageViewModel : PageViewModelBase
+    {
+        public override string Title => "Главная";
+    }
+}
